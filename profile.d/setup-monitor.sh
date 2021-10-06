2c2dc5d1a55b6d6c6a47ac20916edd35114f1572
qubetzl <56742499+qubetzl@users.noreply.github.com>
2021-10-07 01:35:38 +0300

    Added a note about different approach for configuring monitors

--- profile.d/setup-monitor.sh
+++ profile.d/setup-monitor.sh
@@ -1,3 +1,10 @@
+# This script relies on static configuration, 
+# however that is not always suitable.
+# 
+# Different approach for configuring monitors with xrandr:
+# https://github.com/codingtony/udev-monitor-hotplug
+# https://stackoverflow.com/questions/5469828/how-to-create-a-callback-for-monitor-plugged-on-an-intel-graphics
+
 # settings.sh.local is intended to be a Symbolic link
 # to a settings file under `monitor` directory.
 if [ -f settings.sh.local ]; then
