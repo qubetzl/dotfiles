6e5065585d0a75ebb78f92b3702a718ee33b40c5
qubetzl <56742499+qubetzl@users.noreply.github.com>
2022-06-23 20:29:12 +0300

    Updated workstation monitor config
    Both monitors are now attached via DisplayPort, so the Xrandr config
    should also be updated.

--- monitor/workstation.sh
+++ monitor/workstation.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Disabled monitors
-xrandr --output DisplayPort-1 --off
+xrandr --output HDMI-A-0 --off
 xrandr --output DisplayPort-2 --off
 
 #dpi=163
 #xrandr --dpi "${dpi}"
-xrandr --output DisplayPort-0 --primary --mode 3840x2160 --rotate normal --left-of HDMI-A-0
-xrandr --output HDMI-A-0 --mode 3840x2160 --rotate normal
+xrandr --output DisplayPort-0 --primary --mode 3840x2160 --rotate normal
+xrandr --output DisplayPort-1 --mode 3840x2160 --rotate normal --right-of DisplayPort-0
 
