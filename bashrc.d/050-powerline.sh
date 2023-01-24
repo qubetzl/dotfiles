c51a9efc0d6376d1675b9f0e883464f098422d01
qubetzl <56742499+qubetzl@users.noreply.github.com>
2023-01-25 01:24:15 +0200

    Don't try to setup powerline if it is not installed

--- bashrc.d/050-powerline.sh
+++ bashrc.d/050-powerline.sh
@@ -1,6 +1,8 @@
 #powerline statusline plugin
-powerline-daemon -q
-POWERLINE_BASH_CONTINUATION=1
-POWERLINE_BASH_SELECT=1
-repository_root="/usr/share"
-. ${repository_root}/powerline/bindings/bash/powerline.sh
+if command -v powerline-daemon &> /dev/null; then
+    powerline-daemon -q
+    POWERLINE_BASH_CONTINUATION=1
+    POWERLINE_BASH_SELECT=1
+    repository_root="/usr/share"
+    . ${repository_root}/powerline/bindings/bash/powerline.sh
+fi
